2263cc82c4d4369c14c2ab52532f6c4190caa67b
HyacinthViolet <raywong.chn@gmail.com>
2022-06-07 18:57:30 +0800

    sync with ubuntu

--- rscripts/seurat_to_anndata_f.R
+++ rscripts/seurat_to_anndata_f.R
@@ -12,33 +12,60 @@
 #' @param out_dir Directory for output of the intermediate files. A folder named `/tmp` will be created.
 #' @param embeddings Character vector specifying the embeddings of non-linear dimension reductions to 
 #' save, i.e. c("tsne","umap").
+#' @param assay Assay to get expression data, by default "RNA".
 #' @param slot Slot to save. By default the count matrix, i.e. "counts".
 #' @param dr Character vector specifying the dimension reduction to save, by default "pca".
 #' 
 #' @author Sam Morabito
 #' @references https://smorabit.github.io/tutorials/8_velocyto/
 #' 
+#' @importFrom SeuratDisk Loadh5Seurat
+#' @importFrom Seurat Embeddings GetAssayData
+#' @importFrom Matrix writeMM
+#' 
 SeuratToAnndata <- function(
     f_name,
     f_path,
     out_dir,
     embeddings = NULL,
+    assay = "RNA",
     slot = "counts",
-    dr = "pca"
+    dim.red = "pca"
 ){
+  out_dir = file.path(out_dir,"tmp")
   if(!dir.exists(out_dir)) dir.create(out_dir, recursive = T)
+  embeddings = embeddings %||% "umap"
   
   # assuming that you have some Seurat object called seurat_obj:
   seurat_obj <- LoadH5Seurat(paste0(f_path, f_name, ".h5Seurat"))
   
-  # save metadata table:
+  # save metadata table
   seurat_obj[['cell_id']] <- colnames(seurat_obj)
+  write.csv(seurat_obj@meta.data, file=file.path(out_dir,paste0(f_name,'_metadata.csv')), quote=F, row.names=F)
   
-  seurat_obj[[""]] <- seurat_obj@reductions$umap@cell.embeddings[,1]
-  seurat_obj$UMAP_2 <- seurat_obj@reductions$umap@cell.embeddings[,2]
-  seurat_obj$UMAPint_1 <- seurat_obj@reductions$umap.int@cell.embeddings[,1]
-  seurat_obj$UMAPint_2 <- seurat_obj@reductions$umap.int@cell.embeddings[,2]
-  write.csv(seurat_obj@meta.data, file=paste0(out_dir,f_name,'_metadata.csv'), quote=F, row.names=F)
+  # save embeddings
+  lapply(X = embeddings, FUN = function(x){
+    write.csv(
+      x = Embeddings(object = seurat_obj, reduction = x), 
+      file = file.path(out_dir,paste0('embeddings_',x,'.csv')),
+      quote = F, row.names = F
+      )
+  })
+  # emb <- do.call(what = cbind, args = emb)
+  # write.csv(emb, file=file.path(out_dir,paste0(f_name,'_embeddings.csv')), quote=F, row.names=F)
   
+  # write expression counts matrix
+  counts_mtx <- GetAssayData(object = seurat_obj, assay = assay, slot = slot)
+  writeMM(obj = counts_mtx, file = file.path(out_dir, paste0(f_name,'_counts.mtx')))
   
+  # write dimensionality reduction matrix
+  lapply(X = dim.red, FUN = function(x){
+    write.csv(
+      x = Embeddings(object = seurat_obj, reduction = x), 
+      file = file.path(out_dir,paste0('dr_',x,'.csv')),
+      quote = F, row.names = F
+    )
+  })
+  # dr <- do.call(what = cbind, args = dr)
+  # write.csv(dr, file=file.path(out_dir,paste0(f_name,'_dr.csv')), quote=F, row.names=F)
 }
